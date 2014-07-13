c1ff75e5b5f9bdbb3ce8b14ecb50c8dd6786d010
whdeveloper <whdeveloper@gmail.com>
2014-07-14 00:37:41 +0200

    The SettingsActivity

---
+++ src/com/whd/wifikeyview/preferences/SettingsActivity.java
@@ -0,0 +1,30 @@
+package com.whd.wifikeyview.preferences;
+
+import java.util.List;
+
+import com.whd.wifikeyview.R;
+import com.whd.wifikeyview.preferences.fragments.DebugPreferences;
+
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+
+public class SettingsActivity extends PreferenceActivity {
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		// TODO
+	}
+	
+	@Override
+	public void onBuildHeaders(List<Header> target) {
+		loadHeadersFromResource(R.xml.preference_headers, target);
+	}
+	
+	protected boolean isValidFragment(String fragmentName) {
+		return (
+				DebugPreferences.class.getName().equals(fragmentName)
+		);
+	}
+}
