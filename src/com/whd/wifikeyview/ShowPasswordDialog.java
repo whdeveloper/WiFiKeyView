1b7b31ccc17e69ff70ea08890e4d8406b03f4b1e
whdeveloper <whdeveloper@gmail.com>
2014-07-14 00:40:22 +0200

    In progress fix for showing wifi password
    This is an experiment, for getting the password an dialog will be shown
    to the user, after the task that is started by the dialog has finished
    the dialog will be updated. This will fix users not seeing anything.

---
+++ src/com/whd/wifikeyview/ShowPasswordDialog.java
@@ -0,0 +1,7 @@
+package com.whd.wifikeyview;
+
+import android.app.DialogFragment;
+
+public class ShowPasswordDialog extends DialogFragment {
+//	TODO https://github.com/culmor30/android-fragments-example/blob/master/src/com/culmor30/fragmentsTesting/FragmentsTestingActivity.java
+}
