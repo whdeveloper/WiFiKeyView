9bfa2feebfa75a22d5dfbb4332772a5dacf98f73
whdeveloper <whdeveloper@gmail.com>
2014-07-14 00:36:22 +0200

    New hook for the modify network dialog

---
+++ src/com/whd/wifikeyview/hooks/ModifyNetworkHook.java
@@ -0,0 +1,17 @@
+package com.whd.wifikeyview.hooks;
+
+import de.robv.android.xposed.XC_MethodHook;
+
+public class ModifyNetworkHook extends XC_MethodHook {
+	
+	@Override
+	public void beforeHookedMethod(MethodHookParam param) {
+		
+	}
+	
+	@Override
+	public void afterHookedMethod(MethodHookParam param) {
+		
+	}
+	
+}
